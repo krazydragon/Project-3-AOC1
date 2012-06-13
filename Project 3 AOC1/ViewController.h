a3510c8dd4fcc2ceeaa3d7ee4647a615a5e2b39d
Ronaldo Barnes <krazydragon@fullsail.edu>
2012-06-13 13:48:33 -0700

    Started project 3

---
+++ Project 3 AOC1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project 3 AOC1
+//
+//  Created by Ronaldo Barnes on 6/13/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Project 3 AOC1Tests/Project_3_AOC1Tests.m
@@ -0,0 +1,32 @@
+//
+//  Project_3_AOC1Tests.m
+//  Project 3 AOC1Tests
+//
+//  Created by Ronaldo Barnes on 6/13/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import "Project_3_AOC1Tests.h"
+
+@implementation Project_3_AOC1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Project 3 AOC1Tests");
+}
+
+@end
